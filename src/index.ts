95e60eb2389dc29cf2f5ab52eaef8c59a6e1339c
kelly <guojiyou40020@hotmail.com>
2021-11-22 23:48:21 +0800

    added error handling

--- src/index.ts
+++ src/index.ts
@@ -26,36 +26,40 @@ async function getData() {
   let counter = 0;
   for (const repo of repos.data) {
     counter += 1;
-    const repoData = await octokit.request(
-      "GET /repos/{owner}/{repo}/stats/contributors",
-      {
-        owner: username,
-        repo: repo.name,
-      }
-    );
-    console.log(
-      `processing repo: ${repo.name}, ${counter}/${repos.data.length}`
-    );
-    const data = repoData.data as any as RepoContributionsType;
-    for (const repoContrib of data) {
-      // exclude other's repos
-      if (repoContrib.author.login !== "guo40020") {
-        continue;
-      }
-      const reduced = repoContrib.weeks.reduce(
-        (prev, curr) => {
-          return {
-            a: prev.a + curr.a,
-            d: prev.d + curr.d,
-            c: prev.c + curr.c,
-          };
-        },
-        { a: 0, d: 0, c: 0 }
+    try {
+      const repoData = await octokit.request(
+        "GET /repos/{owner}/{repo}/stats/contributors",
+        {
+          owner: username,
+          repo: repo.name,
+        }
       );
+      console.log(
+        `processing repo: ${repo.name}, ${counter}/${repos.data.length}`
+      );
+      const data = repoData.data as any as RepoContributionsType;
+      for (const repoContrib of data) {
+        // exclude other's repos
+        if (repoContrib.author.login !== "guo40020") {
+          continue;
+        }
+        const reduced = repoContrib.weeks.reduce(
+          (prev, curr) => {
+            return {
+              a: prev.a + curr.a,
+              d: prev.d + curr.d,
+              c: prev.c + curr.c,
+            };
+          },
+          { a: 0, d: 0, c: 0 }
+        );
 
-      totalChanges.additions += reduced.a;
-      totalChanges.deletions += reduced.d;
-      totalChanges.commits += reduced.c;
+        totalChanges.additions += reduced.a;
+        totalChanges.deletions += reduced.d;
+        totalChanges.commits += reduced.c;
+      }
+    } catch (error) {
+      console.warn(`get repo detail failed, skipping... repo: ${repo.name}`);
     }
 
     // wait for 500ms to avoid triggering rate limit
@@ -67,4 +71,9 @@ async function getData() {
   console.log(totalChanges);
 }
 
-getData();
+try {
+  getData();
+} catch (error) {
+  console.error(error);
+  process.exit(-1);
+}
